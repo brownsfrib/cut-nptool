571d2b573e2a4228b2f0b3d0c3733c8e5abe9315
Samuel Brown <browns@frib.msu.edu>
2025-08-26 10:26:57 -0400

    forgot some stuff

---
+++ .rootlogon.C
@@ -0,0 +1,10 @@
+
+// Load in whatever libraries you want 
+//R__LOAD_LIBRARY($basePath/nptool/libMicroballAnalysis.so)
+
+void _rootlogon() {
+	//gStyle->SetPalette(kDeepSea);
+	//std::string basePath(gSystem->Getenv("basePath"));
+	//std::string libInclude = basePath + "/lib/include";
+	//gInterpreter->AddIncludePath(libInclude.c_str());
+}
